d83bc58ddacd0ae670e62cdffae3b9ea5aa27ce7
Kamil Toporek <kamil.toporek98@gmail.com>
2024-03-21 10:05:09 +0100

    Add summ of goods

--- task.py
+++ task.py
@@ -2,7 +2,11 @@ shopp_list = {
     "piekarnia" : ["chleb", "pączek", "bułki"],
     "warzywniak" : ["marchew", "seler", "rukola"]
 }
+print("Lista zakupów") 
+sum_produkt = 0
 for sklep, produkty in shopp_list.items():
     sklep1 = sklep.capitalize()
     produkty1 = [produkt.capitalize() for produkt in produkty]
     print(f"Idę do {sklep1}, kupuję tu następujące rzeczy: {', '.join(produkty1)}.")
+    sum_produkt += len(produkty1) 
+print(f"W sumię kupię {sum_produkt} produktów.")
