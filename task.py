47478e75081a5af6358fe342cda8c8ac5d102ad4
Kamil Toporek <kamil.toporek98@gmail.com>
2024-03-21 10:02:31 +0100

    Add loop for and iterate through dict

--- task.py
+++ task.py
@@ -2,3 +2,7 @@ shopp_list = {
     "piekarnia" : ["chleb", "pączek", "bułki"],
     "warzywniak" : ["marchew", "seler", "rukola"]
 }
+for sklep, produkty in shopp_list.items():
+    sklep1 = sklep.capitalize()
+    produkty1 = [produkt.capitalize() for produkt in produkty]
+    print(f"Idę do {sklep1}, kupuję tu następujące rzeczy: {', '.join(produkty1)}.")
